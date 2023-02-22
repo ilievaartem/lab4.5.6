859838605dd3106bcc6a6efd636c42a5ca63ccc4
Artem Iliev <artemilev05@gmail.com>
2023-02-22 23:57:07 +0200

    Third

--- src/lab4/exercise/Third.java
+++ src/lab4/exercise/Third.java
@@ -6,13 +6,29 @@ import java.io.InputStreamReader;
 public class Third {
     public void Third() throws IOException {
         BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(scanner.readLine());
-        int b = Integer.parseInt(scanner.readLine());
-        int c = Integer.parseInt(scanner.readLine());
-        if (a < c + b && b < a + c && c < b + a){
-            System.out.println("Такий трикутник існує");
-        } else {
-            System.out.println("Ведіть праивльно числа наново");
+        while (true) {
+            int a = Integer.parseInt(scanner.readLine());
+            int b = Integer.parseInt(scanner.readLine());
+            int c = Integer.parseInt(scanner.readLine());
+            if (a < c + b && b < a + c && c < b + a) {
+                System.out.println("Такий трикутник існує");
+                if (a == Math.sqrt(Math.pow(c, 2) + Math.pow(b, 2)) || b == Math.sqrt(Math.pow(c, 2) + Math.pow(a, 2))
+                        || c == Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))) {
+                    System.out.println("Трикутник прямокутний");
+                    break;
+                } else if (a == b && c == b && c == a) {
+                    System.out.println("Трикутник рівностороній");
+                    break;
+                } else if (a == b || b == c || c == a) {
+                    System.out.println("Трикутник рівнобедрений");
+                    break;
+                } else {
+                    System.out.println("Трикутник довільний");
+                    break;
+                }
+            } else {
+                System.out.println("Ведіть праивльно числа наново");
+            }
         }
     }
 }
